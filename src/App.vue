8faad7ea33f7b82a9247cb6f2ff708c5125e8c7c
Bryan Gordon <bryanbge92@gmail.com>
2025-02-10 14:22:46 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="app">
+    <AppHeader></AppHeader>
+    <AppFooter></AppFooter>
+</div>
+</template>
+
+<style scoped>
+  .app {
+    box-sizing: border-box;
+    color: #37474f;
+    font-family: 'Courier New', Courier, monospace;
+    text-align: center;
+    margin:  0 auto;
+    padding: 0;
+  }
+
+  .container {
+    padding-bottom: 4rem;
+  }
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,46 @@
+<script setup>
+  // Crear todo el contenido de vue router 
+  // (configuracion y declaracion en componentes, Hacer primero esto antes de seguir programando)
+</script>
+
+<template>
+  <header>
+    <h1 class="title">Vue Trello</h1>
+  </header>
+</template>
+
+<style scoped>
+  header {
+    box-sizing: border-box;
+    background-color: #607d8b;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    margin: 0;
+    padding: 1rem;
+
+    & .title {
+      align-self: center;
+      color: #eceff1;
+      font-size: 1.2rem;
+      font-weight: bold;
+      margin-right: 1rem;
+    }
+  }
+
+  .btn-header {
+    background-color: #455a64;
+    border: none;
+    border-radius: 3px;
+    box-shadow: 0 0 0 0.5px rgba(49, 49, 93, .03),
+      0 2px 5px 0 rgba(49, 49, 93, .1),
+      0 1px 2px rgba(0, 0, 0, .08)
+    ;
+    color: #b0bec5;
+    cursor: pointer;
+    text-decoration: none;
+    padding: 0.5rem;
+    margin: 0 0.5rem;
+  }
+
+</style>
