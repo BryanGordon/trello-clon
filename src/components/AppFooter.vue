41bb77ca566641e0b8f256dfaa15792626cb788f
Bryan Gordon <bryanbge92@gmail.com>
2025-02-10 22:44:25 -0500

    Add styles

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,9 +3,19 @@
 </script>
 
 <template>
-
+  <footer>
+    <span>Trello clone</span>
+  </footer>
 </template>
 
 <style scoped>
-
+  footer {
+    box-sizing: border-box;
+    background-color: #cfd8dc;
+    bottom: 0;
+    left: 0;
+    padding: 1rem;
+    position: absolute;
+    width: 100%;
+  }
 </style>
