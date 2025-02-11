dc1a30e3f0e7218dfd5a36bad57c2f93f251eeb1
Bryan Gordon <bryanbge92@gmail.com>
2025-02-10 22:46:16 -0500

    Create Home component

---
+++ src/components/views/Home.vue
@@ -0,0 +1,20 @@
+<script setup>
+    font-size: 1.1rem;
+    outline: 0;
+    padding: 0.5rem;
+    transition: all 600ms ease;
+
+  }
+
+  input:focus
+
+  input:active {
+    background-color: whitesmoke;
+    color: #546e7a;
+  }
+
+  input::placeholder {
+    color: whitesmoke;
+  }
+
+</style>
