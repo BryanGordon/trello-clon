95046d3cb65d6163a6a8d6a478520fee79e26963
Bryan Gordon <bryanbge92@gmail.com>
2025-02-11 22:27:36 -0500

    Update app routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/views/Home.vue'
+import Board from '../components/views/Board.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home 
+  },
+  {
+    path: '/board/:id',
+    name: 'board',
+    component: Board,
+    props: true
   }
 ]
 
